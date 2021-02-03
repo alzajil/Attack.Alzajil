6e2cf0a5ec668c4ad4b11e7f48a5b4e866e47390
A_Z@Griffin_Iraq <78331807+alzajil@users.noreply.github.com>
2021-02-03 10:48:38 +0300

    Add files via upload

---
+++ Attack.facebook.py
@@ -0,0 +1,63 @@
+#D1
+def al():
+
+
+ print ("                            _ - -_   ")
+print  ("                           /         -)")
+print("                          / ___   | ")
+print("     ____ ----- -= ~~ // / | | | | | ~~~ == -----_____")
+print("    //~// // // // //  // ~ / | | // | | | | | \\\\\\\\\\\ ~\\")
+print("   ////.// // // // // // // / | | // // // / | \\\\\\\\\\\\\\ \ ")
+print("  / /""// \~\ \\\\~~~~~~~~~~~~\ |.| |   / ~~~~~~~~~~~~~~~~~\\/\\ ")
+print(" / /                      \ |.| | /                     \ \\")
+print("/~/                       // ~ /  \\                      \~\ ")
+print("/                        // / W ^\ \\                       \ ")
+print("                        //~// | | \ \\")
+print("                       ~~~~~~~~~~~~~~~")
+print("~~~~~~~~~~~~~~~~~~~~~~~A_Z@GRIFFIN_IRAQ~~~~~~~~~~~~~~~~~~~~~~~~~~")
+#----------------------------------------------------------------------------------------------------------
+#D2
+user =input("Enter the victim's link : ")
+print(user)
+ #--------
+#db
+
+#
+db_username= "admin"
+db_password ="1111"
+
+user=input("Enter your email or account ID :")
+password=input("Enter your password : ")
+
+if user ==db_username and password ==db_password:
+ print("Signed in ")
+else:
+ print("The victim link entry interface is loaded")
+
+ al()
+#---------------------------------------------------------------------------------------------------------
+ #D3
+
+
+#----------------------------------------------------------------------------------------------------------
+#D4
+tak =0
+while tak < 5 :
+ print("Successful attack")
+ print("Successful attack")
+ print("Successful attack")
+ print("Successful attack")
+ print("The connection is restored")
+ print("[][][][][][][][][][][][][][][][]")
+ print("Successful attack")
+ print("Successful attack")
+ print("Successful attack")
+ print("Successful attack")
+ print("Successful attack")
+ print("Successful attack")
+ print("Successful attack")
+ print("Successful attack")
+ print("Successful attack")
+ print("Successful attack")
+ print("Successful attack")
+ print("Successful attack")
